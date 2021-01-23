84882d3076780e0f79807cd2b97ae5b6c906f9e0
PedroContessoto1 <49294220+PedroContessoto1@users.noreply.github.com>
2021-01-22 23:03:00 -0300

    Adicionar arquivos de projeto.

---
+++ Zombie_block_jokerxblack/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Zombie_block_jokerxblack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Lime;
+            this.checkBox1.Location = new System.Drawing.Point(90, 100);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Pistol_ammo";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(54, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zombie block not connected";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(298, 146);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Zombie_block_jokerxblack/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace Zombie_block_jokerxblack
+{
+    public partial class Form1 : Form
+    {
+        Mem meme = new Mem();
+        public static string Pistol_ammo = "mono.dll+0x001F3648,550,5b8,10";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int PID = meme.GetProcIdFromName("Zumbi blocks ultimate");
+            if(PID > 0)
+            {
+                label1.Visible = false;
+                meme.OpenProcess(PID);
+                Thread WA = new Thread(writeAmmo) { IsBackground = true};
+                WA.Start();
+            }
+        }
+
+        private void writeAmmo()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    meme.WriteMemory(Pistol_ammo,"int","18");
+                    Thread.Sleep(2);
+                }
+                Thread.Sleep(2);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
